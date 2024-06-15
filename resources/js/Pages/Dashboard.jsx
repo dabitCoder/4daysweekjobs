9fbd98c2511cb4a5b18ddfd295a2de1272e8a260
David Oliva <doliva@icopartners.com>
2024-06-15 16:18:59 +0200

    Show is active

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -44,12 +44,18 @@ export default function Dashboard({ auth }) {
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                         {userJobs.map(job => (
                             <div key={job.id} className="relative bg-white overflow-hidden shadow-sm sm:rounded-lg group">
-                                <div className="p-6 text-gray-900 group-hover:opacity-25 transition-opacity duration-300">
+                                <div
+                                    className="p-6 text-gray-900 group-hover:opacity-25 transition-opacity duration-300">
                                     <p className="text-gray-700 mb-2"><strong>Title:</strong> {job.title}</p>
-                                    <p className="text-gray-600 mb-2"><strong>Salary:</strong> {job.salary_range === "0" ? 'No specified' : job.salary_range}</p>
-                                    <p className="text-gray-600 mb-2"><strong>Description:</strong> {job.description}</p>
+                                    <p className="text-gray-600 mb-2">
+                                        <strong>Salary:</strong> {job.salary_range === "0" ? 'No specified' : job.salary_range}
+                                    </p>
+                                    <p className="text-gray-600 mb-2"><strong>Description:</strong> {job.description}
+                                    </p>
+                                    <p className="text-gray-700 mb-2"><strong>Is Active:</strong> {job.is_active ? 'Yes' : 'No'}</p>
                                 </div>
-                                <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                                <div
+                                    className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                                     <PrimaryButton className="text-white text-xl font-bold bg-black bg-opacity-50 px-4 py-2 rounded">EDIT</PrimaryButton>
                                 </div>
                             </div>
