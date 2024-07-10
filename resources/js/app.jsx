3f68eb3e33bce7e0f83a431ff5cff40821e383d6
doliva <doliva@icopartners.com>
2024-07-10 23:48:13 +0200

    Google analytics

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,22 +4,24 @@ import "../css/app.css";
 import { createRoot } from "react-dom/client";
 import { createInertiaApp } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import ReactGA from "react-ga4";
 
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
-	title: (title) => `${title} - ${appName}`,
-	resolve: (name) =>
-		resolvePageComponent(
-			`./Pages/${name}.jsx`,
-			import.meta.glob("./Pages/**/*.jsx"),
-		),
-	setup({ el, App, props }) {
-		const root = createRoot(el);
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.jsx`,
+            import.meta.glob("./Pages/**/*.jsx")
+        ),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+        ReactGA.initialize("G-V4GZWF4FY2");
 
-		root.render(<App {...props} />);
-	},
-	progress: {
-		color: "#4B5563",
-	},
+        root.render(<App {...props} />);
+    },
+    progress: {
+        color: "#4B5563",
+    },
 });
--- resources/js/ssr.jsx
+++ resources/js/ssr.jsx
@@ -1,16 +1,22 @@
-import { createInertiaApp } from '@inertiajs/react'
-import createServer from '@inertiajs/react/server'
-import ReactDOMServer from 'react-dom/server'
+import { createInertiaApp } from "@inertiajs/react";
+import createServer from "@inertiajs/react/server";
+import ReactDOMServer from "react-dom/server";
 import React from "react";
+import ReactGA from "react-ga4";
 
-createServer(page =>
-  createInertiaApp({
-    page,
-    render: ReactDOMServer.renderToString,
-    resolve: name => {
-      const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
-      return pages[`./Pages/${name}.jsx`]
-    },
-    setup: ({ App, props }) => <App {...props} />,
-  }),
-)
+createServer((page) =>
+    createInertiaApp({
+        page,
+        render: ReactDOMServer.renderToString,
+        resolve: (name) => {
+            const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
+            return pages[`./Pages/${name}.jsx`];
+        },
+        setup: ({ el, App, props }) => {
+            ReactGA.initialize("G-V4GZWF4FY2");
+            const root = createRoot(el);
+
+            root.render(<App {...props} />);
+        },
+    })
+);
